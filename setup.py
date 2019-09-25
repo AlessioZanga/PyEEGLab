8bba6c8ac3052e96c8d99a37dbc56492528fe818
AlessioZanga <alessio.zanga@outlook.it>
2019-09-25 13:30:01 +0200

    Update to version 0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='PyEEGLab',
-    version='0.3.0',
+    version='0.4.0',
     author='Alessio Zanga',
     author_email='alessio.zanga@outlook.it',
     license='GNU GENERAL PUBLIC LICENSE - Version 3, 29 June 2007',
