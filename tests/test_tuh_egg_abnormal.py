88e4687c9693b8dc89ed00aac1c6f2aa8dfa9dd4
AlessioZanga <alessio.zanga@outlook.it>
2019-11-30 14:16:46 +0100

    Update tuh_eeg_abnormal tests

---
+++ tests/test_tuh_egg_abnormal.py
@@ -0,0 +1,21 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pyeeglab
+
+PATH = './tests/samples/tuh_eeg_abnormal/edf'
+
+def test_index():
+    pyeeglab.TUHEEGAbnormalLoader(PATH)
+
+def test_loader():
+    loader = pyeeglab.TUHEEGAbnormalLoader(PATH)
+    print(loader.get_dataset())
+    print(loader.get_dataset_text())
+    print(loader.get_channelset())
+    print(loader.get_lowest_frequency())
+
+def test_dataset():
+    dataset = pyeeglab.TUHEEGAbnormalDataset(PATH, frames=8)
+    dataset.load('graphs', 0.7, 25, 75, True)
