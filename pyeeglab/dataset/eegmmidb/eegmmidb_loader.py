cbec3c5c564fef2e8363cb9bb698c27494258695
AlessioZanga <alessio.zanga@outlook.it>
2020-01-06 15:45:12 +0100

    Add exclude_frequency in loader

--- pyeeglab/dataset/eegmmidb/eegmmidb_loader.py
+++ pyeeglab/dataset/eegmmidb/eegmmidb_loader.py
@@ -1,11 +1,14 @@
 import logging
 
+from typing import List
+
 from ...io import DataLoader
 from .eegmmidb_index import EEGMMIDBIndex
 
 
 class EEGMMIDBLoader(DataLoader):
-    def __init__(self, path: str) -> None:
-        super().__init__(path)
+
+    def __init__(self, path: str, exclude_frequency: List[int] = [128]) -> None:
+        super().__init__(path, exclude_frequency = exclude_frequency)
         logging.debug('Create EEG Motor Movement/Imagery Loader')
         self.index = EEGMMIDBIndex(self.path)
