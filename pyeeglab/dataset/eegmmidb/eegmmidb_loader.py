3d1dace7d7439343be5b70ead129a9464972f004
AlessioZanga <alessio.zanga@outlook.it>
2020-01-06 02:29:47 +0100

    Remove unused channel ref filters

--- pyeeglab/dataset/eegmmidb/eegmmidb_loader.py
+++ pyeeglab/dataset/eegmmidb/eegmmidb_loader.py
@@ -33,13 +33,10 @@ class EEGMMIDBLoader(DataLoader):
         edf.crop(e.begin, e.end-e.begin)
         return edf
 
-    def get_dataset(self, exclude_channel_ref: List[str] = ['02_tcp_le', '03_tcp_ar_a']) -> List[RawEDF]:
+    def get_dataset(self) -> List[RawEDF]:
         files = self.index.db.query(File, Event)
         files = files.filter(File.id == Event.file_id)
-        files = files.filter(
-            File.format == 'edf',
-            ~File.channel_ref.in_(exclude_channel_ref)
-        ).all()
+        files = files.filter(File.format == 'edf').all()
         pool = Pool(len(sched_getaffinity(0)))
         edfs = pool.starmap(self._get_dataset_by_event, files)
         pool.close()
@@ -53,10 +50,9 @@ class EEGMMIDBLoader(DataLoader):
         txts = {f.id: (join(self.index.path, f.path), f.label) for f in txts}
         return txts
 
-    def get_channelset(self, exclude: List[str] = ['02_tcp_le', '03_tcp_ar_a']) -> List[str]:
+    def get_channelset(self) -> List[str]:
         edfs = self.index.db.query(File, Metadata)
         edfs = edfs.filter(File.id == Metadata.id)
-        edfs = edfs.filter(~File.channel_ref.in_(exclude))
         edfs = edfs.group_by(Metadata.channels).all()
         edfs = [edf[1] for edf in edfs]
         edfs = [set(json.loads(edf.channels)) for edf in edfs]
