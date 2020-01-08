e2f0e38887fd2723d0e68cde32e82fc838863cdf
AlessioZanga <alessio.zanga@outlook.it>
2020-01-08 17:55:17 +0100

    Refactoring datasets classes

--- pyeeglab/dataset/eegmmidb/eegmmidb_dataset.py
+++ pyeeglab/dataset/eegmmidb/eegmmidb_dataset.py
@@ -1,6 +1,3 @@
-from typing import List
-import numpy as np
-
 from .eegmmidb_loader import EEGMMIDBLoader
 from ..dataset import Dataset
 from ...preprocessing import Preprocessor
@@ -9,6 +6,7 @@ from ...preprocessing import Preprocessor
 class EEGMMIDBDataset(Dataset):
 
     def __init__(self, path: str = './data/physionet.org/files/eegmmidb/1.0.0/', frames: int = 8) -> None:
+        super().__init__()
         self.loader = EEGMMIDBLoader(path)
         self.dataset = self.loader.get_dataset()
         self.labels = [data.label for data in self.dataset]
@@ -19,31 +17,3 @@ class EEGMMIDBDataset(Dataset):
             self.loader.get_lowest_frequency(),
             frames
         )
-
-    def get_channels(self, drop_channels: List[str]) -> List[str]:
-        channels = list(set(self.loader.get_channelset()) - set(drop_channels))
-        channels = sorted(channels)
-        return channels
-
-    def set_bandpass_frequency(self, l_freq: float, h_freq: float) -> None:
-        self.preprocessor.set_bandpass_frequency(l_freq, h_freq)
-
-    # Modes: frames, correlations, adjs, weighted_adjs, graphs
-
-    def load(self, mode: str = 'adjs', c: float = 0.7, p1: int = 25, p2: int = 75, node_features: bool = False, export: str = None):
-        dataset = self.preprocessor.load(
-            mode,
-            self.dataset,
-            self.labels,
-            c,
-            p1,
-            p2,
-            node_features,
-            export
-        )
-        data = dataset['data']
-        labels = dataset['labels']
-        if mode != 'graphs':
-            data = np.array(data).astype('float32')
-        labels = np.array(labels).astype('int32')
-        return data, labels
--- pyeeglab/dataset/tuh_eeg_artifact/tuh_eeg_artifact_dataset.py
+++ pyeeglab/dataset/tuh_eeg_artifact/tuh_eeg_artifact_dataset.py
@@ -1,5 +1,4 @@
 from typing import List
-import numpy as np
 
 from .tuh_eeg_artifact_loader import TUHEEGArtifactLoader
 from ..dataset import Dataset
@@ -9,6 +8,7 @@ from ...preprocessing import Preprocessor
 class TUHEEGArtifactDataset(Dataset):
 
     def __init__(self, path: str = './data/tuh_eeg_artifact/v1.0.0/edf/', drop_channels: List[str] = ['IBI', 'BURSTS', 'STI 014'], frames: int = 8) -> None:
+        super().__init__()
         self.loader = TUHEEGArtifactLoader(path)
         self.dataset = self.loader.get_dataset()
         self.labels = [data.label for data in self.dataset]
@@ -19,31 +19,3 @@ class TUHEEGArtifactDataset(Dataset):
             self.loader.get_lowest_frequency(),
             frames
         )
-
-    def get_channels(self, drop_channels: List[str]) -> List[str]:
-        channels = list(set(self.loader.get_channelset()) - set(drop_channels))
-        channels = sorted(channels)
-        return channels
-
-    def set_bandpass_frequency(self, l_freq: float, h_freq: float) -> None:
-        self.preprocessor.set_bandpass_frequency(l_freq, h_freq)
-
-    # Modes: frames, correlations, adjs, weighted_adjs, graphs
-
-    def load(self, mode: str = 'adjs', c: float = 0.7, p1: int = 25, p2: int = 75, node_features: bool = False, export: str = None):
-        dataset = self.preprocessor.load(
-            mode,
-            self.dataset,
-            self.labels,
-            c,
-            p1,
-            p2,
-            node_features,
-            export
-        )
-        data = dataset['data']
-        labels = dataset['labels']
-        if mode != 'graphs':
-            data = np.array(data).astype('float32')
-        labels = np.array(labels).astype('int32')
-        return data, labels
