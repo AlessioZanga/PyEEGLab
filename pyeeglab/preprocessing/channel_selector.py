c160ff147a3ae476eb666392399e489bd09eec5a
AlessioZanga <alessio.zanga@outlook.it>
2020-01-10 21:04:10 +0100

    Update parameter name to blacklist

--- pyeeglab/preprocessing/channel_selector.py
+++ pyeeglab/preprocessing/channel_selector.py
@@ -9,10 +9,10 @@ from .pipeline import Preprocessor
 
 class CommonChannelSet(Preprocessor):
 
-    def __init__(self, drop_channels: List[str] = []) -> None:
+    def __init__(self, blacklist: List[str] = []) -> None:
         super().__init__()
         logging.debug('Create common channel_set preprocessor')
-        self.drop_channels = drop_channels
+        self.blacklist = blacklist
 
     def to_json(self) -> str:
         json = {self.__class__.__name__ : {}}
@@ -20,7 +20,7 @@ class CommonChannelSet(Preprocessor):
         return json
 
     def run(self, data: Raw, **kwargs) -> Raw:
-        channels = set(kwargs['channel_set']) - set(self.drop_channels)
+        channels = set(kwargs['channel_set']) - set(self.blacklist)
         channels = list(channels)
         data.set_channels(channels)
         return data
