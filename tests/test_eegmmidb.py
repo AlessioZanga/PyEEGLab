96033e8d9d30d38af5f843e5e875e4b38bdba12e
AlessioZanga <alessio.zanga@outlook.it>
2020-01-13 14:21:10 +0100

    Refactoring tests

---
+++ tests/test_eegmmidb.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import unittest
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from pyeeglab import TextMiner, EEGMMIDBLoader, EEGMMIDBDataset, \
+                     Pipeline, CommonChannelSet, LowestFrequency, BandPassFrequency, ToDataframe, \
+                     DynamicWindow, ToNumpy
+
+class TestEEGMMIDB(unittest.TestCase):
+    PATH = './tests/samples/physionet.org/files/eegmmidb/1.0.0'
+
+    def test_index(self):
+        EEGMMIDBLoader(self.PATH)
+
+    def test_loader(self):
+        loader = EEGMMIDBLoader(self.PATH)
+        loader.get_dataset()
+        loader.get_dataset_text()
+        loader.get_channelset()
+        loader.get_lowest_frequency()
+
+    def test_dataset(self):
+        dataset = EEGMMIDBDataset(self.PATH)
+        preprocessing = Pipeline([
+            CommonChannelSet(),
+            LowestFrequency(),
+            BandPassFrequency(0.1, 47),
+            ToDataframe(),
+            DynamicWindow(4),
+            ToNumpy()
+        ])
+        dataset = dataset.set_pipeline(preprocessing).load()
+
+    def test_text_miner(self):
+        loader = EEGMMIDBLoader(self.PATH)
+        text = loader.get_dataset_text()
+        miner = TextMiner(text)
+        miner.get_dataset()
