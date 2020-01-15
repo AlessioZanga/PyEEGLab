832c1cffe4dfaf9afba94229ba7c100de4477fac
AlessioZanga <alessio.zanga@outlook.it>
2020-01-15 19:35:35 +0100

    Update to 0.7.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='PyEEGLab',
-    version='0.7.0',
+    version='0.7.1',
     author='Alessio Zanga',
     author_email='alessio.zanga@outlook.it',
     license='GNU GENERAL PUBLIC LICENSE - Version 3, 29 June 2007',
