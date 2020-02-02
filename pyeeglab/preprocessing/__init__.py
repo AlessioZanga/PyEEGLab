d1a9e2524f2c2037b0fc933a51c2386b1972f4bb
AlessioZanga <alessio.zanga@outlook.it>
2020-02-02 22:27:33 +0100

    Add static and overlapping frame generators

--- pyeeglab/preprocessing/__init__.py
+++ pyeeglab/preprocessing/__init__.py
@@ -3,6 +3,6 @@ from .channel_selector import CommonChannelSet
 from .frequency_selector import LowestFrequency
 from .filter_selector import BandPassFrequency
 from .data_converter import ToDataframe, ToNumpy, CorrelationToAdjacency
-from .frame_generator import DynamicWindow
+from .frame_generator import StaticWindow, DynamicWindow, StaticWindowOverlap, DynamicWindowOverlap
 from .brain_connectivity import SpearmanCorrelation, BinarizedSpearmanCorrelation, Bandpower
 from .graph_generator import GraphGenerator, GraphWithFeatures
--- pyeeglab/preprocessing/frame_generator.py
+++ pyeeglab/preprocessing/frame_generator.py
@@ -8,12 +8,64 @@ from pandas import DataFrame
 from .pipeline import Preprocessor
 
 
+class StaticWindow(Preprocessor):
+
+    def __init__(self, frames: int, length: float) -> None:
+        super().__init__()
+        self.frames = frames
+        self.length = length
+        logging.debug('Create static frames (%d of %d seconds each) generator', frames, length)
+
+    def to_json(self) -> str:
+        json = {
+            self.__class__.__name__ : {
+                'frames': self.frames,
+                'length': self.length
+            }
+        }
+        json = dumps(json)
+        return json
+
+    def run(self, data: DataFrame, **kwargs) -> List[DataFrame]:
+        step = floor(self.length * kwargs['lowest_frequency'])
+        if (step * frames > len(data)):
+            raise RuntimeError('Error while creating static frames: not enough data.')
+        return [data[t:t+step] for t in range(0, step * frames, step)]
+
+
+class StaticWindowOverlap(Preprocessor):
+
+    def __init__(self, frames: int, length: float, overlap: float) -> None:
+        super().__init__()
+        self.frames = frames
+        self.length = length
+        self.overlap = overlap
+        logging.debug('Create static frames (%d of %d seconds each with %.2f overlap) generator', frames, length, overlap)
+
+    def to_json(self) -> str:
+        json = {
+            self.__class__.__name__ : {
+                'frames': self.frames,
+                'length': self.length,
+                'overlap': self.overlap
+            }
+        }
+        json = dumps(json)
+        return json
+
+    def run(self, data: DataFrame, **kwargs) -> List[DataFrame]:
+        step = floor(self.length * kwargs['lowest_frequency'])
+        if (step * frames > len(data)):
+            raise RuntimeError('Error while creating static frames: not enough data.')
+        return [data[t:t+step] for t in range(0, step * frames, floor(step * self.overlap))]
+
+
 class DynamicWindow(Preprocessor):
 
     def __init__(self, frames: int) -> None:
         super().__init__()
         self.frames = frames
-        logging.debug('Create dynamic frames (%d) generator preprocessor', frames)
+        logging.debug('Create dynamic frames (%d) generator', frames)
 
     def to_json(self) -> str:
         json = {
@@ -29,3 +81,28 @@ class DynamicWindow(Preprocessor):
         if self.frames > 1:
             step = floor(step/self.frames)
         return [data[t:t+step] for t in range(0, len(data) - step + 1, step)]
+
+
+class DynamicWindowOverlap(Preprocessor):
+
+    def __init__(self, frames: int, overlap: float) -> None:
+        super().__init__()
+        self.frames = frames
+        self.overlap = overlap
+        logging.debug('Create dynamic frames (%d with %.2f overlap) generator', frames, overlap)
+
+    def to_json(self) -> str:
+        json = {
+            self.__class__.__name__ : {
+                'frames': self.frames,
+                'overlap': self.overlap
+            }
+        }
+        json = dumps(json)
+        return json
+
+    def run(self, data: DataFrame, **kwargs) -> List[DataFrame]:
+        step = len(data)
+        if self.frames > 1:
+            step = floor(step/self.frames)
+        return [data[t:t+step] for t in range(0, len(data) - step + 1, floor(step * overlap))]
