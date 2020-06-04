078f538556d105e4e1665133d7a085bf04586a8e
AlessioZanga <alessio.zanga@outlook.it>
2020-06-04 15:49:20 +0200

    Fix wrong dataset with default channels reference

--- pyeeglab/dataset/tuh_eeg_abnormal/tuh_eeg_abnormal_loader.py
+++ pyeeglab/dataset/tuh_eeg_abnormal/tuh_eeg_abnormal_loader.py
@@ -8,7 +8,7 @@ from typing import List
 
 class TUHEEGAbnormalLoader(DataLoader):
 
-    def __init__(self, path: str, exclude_channels_reference: List[str] = ['02_tcp_le', '02_tcp_ar_a']) -> None:
-        super().__init__(path, exclude_channels_reference=exclude_channels_reference)
+    def __init__(self, path: str) -> None:
+        super().__init__(path)
         logging.debug('Create TUH EEG Corpus Loader')
         self.index = TUHEEGAbnormalIndex(self.path)
