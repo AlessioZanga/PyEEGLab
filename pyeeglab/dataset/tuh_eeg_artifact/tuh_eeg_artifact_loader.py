7195ffe8522452f8896d7e1d1c1614ebe2c11b72
AlessioZanga <alessio.zanga@outlook.it>
2020-08-11 13:12:18 +0200

    Fix #31

--- pyeeglab/dataset/tuh_eeg_artifact/tuh_eeg_artifact_loader.py
+++ pyeeglab/dataset/tuh_eeg_artifact/tuh_eeg_artifact_loader.py
@@ -8,7 +8,7 @@ from .tuh_eeg_artifact_index import TUHEEGArtifactIndex
 
 class TUHEEGArtifactLoader(DataLoader):
 
-    def __init__(self, path: str, exclude_channels_reference: List[str] = ['02_tcp_le', '02_tcp_ar_a']) -> None:
+    def __init__(self, path: str, exclude_channels_reference: List[str] = ['02_tcp_le', '03_tcp_ar_a']) -> None:
         super().__init__(path, exclude_channels_reference=exclude_channels_reference)
         logging.debug('Create TUH EEG Corpus Loader')
         self.index = TUHEEGArtifactIndex(self.path)
